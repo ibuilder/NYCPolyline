76ba7e569fe675abfa3affffeb28ce069b0a3ddd
unknown <mmemma@NYCTAB0009.tcco.org>
2014-11-20 11:08:23 -0500

    Add styling

--- index.php
+++ index.php
@@ -10,11 +10,18 @@
         zoom: 13, // The initial zoom level when your map loads (0-20)
         center: myLatlng, // Centre the Map to our coordinates variable
         mapTypeId: google.maps.MapTypeId.ROADMAP, // Set the type of Map
+        styles: [ {   "featureType":"landscape",    "stylers":[     {       "hue":"#F1FF00"     },      {       "saturation":-27.4      },      {       "lightness":9.4     },      {       "gamma":1     }   ] },  {   "featureType":"road.highway",   "stylers":[     {       "hue":"#0099FF"     },      {       "saturation":-20      },      {       "lightness":36.4      },      {       "gamma":1     }   ] },  {   "featureType":"road.arterial",    "stylers":[     {       "hue":"#00FF4F"     },      {       "saturation":0      },      {       "lightness":0     },      {       "gamma":1     }   ] },  {   "featureType":"road.local",   "stylers":[     {       "hue":"#FFB300"     },      {       "saturation":-38      },      {       "lightness":11.2      },      {       "gamma":1     }   ] },  {   "featureType":"water",    "stylers":[     {       "hue":"#00B6FF"     },      {       "saturation":4.2      },      {       "lightness":-63.4     },      {       "gamma":1     }   ] },  {   "featureType":"poi",    "stylers":[     {       "hue":"#9FFF00"     },      {       "saturation":0      },      {       "lightness":0     },      {       "gamma":1     }   ] }]
       }
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions); // Render our map within the empty div
 
     // Load a GeoJSON from the same server as our demo.
   	map.data.loadGeoJson('manhattan.geojson');
+
+    var featureStyle = {
+      fillColor: '#0088FF',
+      strokeWeight: 1
+    }
+    map.data.setStyle(featureStyle);
          
 }
 google.maps.event.addDomListener(window, 'load', initialise); // Execute our 'initialise' function once the page has loaded. 
